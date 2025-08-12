72d6b24de2af07ae898bab9350c722ea3b90b05f
FantaUva350ml <davi2008rodriguespires@gmail.com>
2025-08-12 09:07:13 -0300

    Class update

--- Classes/a03/my-app/App.js
+++ Classes/a03/my-app/App.js
@@ -1,11 +1,36 @@
-import { View, TouchableOpacity } form 'reac-native';
+import { TouchableOpacity, View } from 'react-native';
+import { styles } from './styles.js';
+
+function botao(){
+  console.log('Botão precionado 1')
+}
+
+function botao2(){
+  console.log('Botão precionado 2')
+}
 
 export default function App() {
-  return(
-  <View> 
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.ladoAlado}>
+
+      <View style={styles.areaInterna}>
+        <TouchableOpacity onPress={botao} style={styles.botao1}></TouchableOpacity>
+      </View>
+      <View style={styles.areaInterna}>
+        <TouchableOpacity onPress={botao2} style={styles.botao2}></TouchableOpacity>
+      </View>
+
+      </View>
+
+
+
       <View></View>
       <View></View>
-      <View></View>  
-  </View>
-  )
+
+    </View>
+  );
 }
+
+
---
+++ Classes/a03/my-app/styles.js
@@ -0,0 +1,26 @@
+
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        
+    },
+    ladoAlado: {
+        flex:1,
+        flexDirection:'row',
+    },
+    areaInterna:{
+        flex:1
+    },
+    botao1:{
+        backgroundColor:'#25df77',
+        width:'100%',
+        height:'100%'
+    },
+    botao2:{
+        backgroundColor:'#25df07',
+        width:'100%',
+        height:'100%'
+    },
+})
